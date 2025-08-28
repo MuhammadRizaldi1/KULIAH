d9d6d881cd6b31ce096a6fa730bfa2d9570fb4ff
MuhammadRizaldi1 <mhmdrizaldi123p@gmail.com>
2025-08-28 13:27:07 +0900

    update kelas 5IF3

---
+++ Tugas_i/bin/5IF3/latihan/latihan1.dart
@@ -0,0 +1,17 @@
+void main() {
+  var nama = "Rizaldi";
+  var prodi = "Informatika";
+  var kelas = "5IF3";
+  int umur = 21;
+  num berat = 55.5;
+  num tinggi = 167;
+  bool isActive = true;
+
+  print("nama saya : $nama");
+  print("Prodi  : $prodi");
+  print("Kelas : $kelas");
+  print("Usia saya : $umur");
+  print("Berat badan : $berat kg");
+  print("Tinggi badan : $tinggi cm");
+  print("Mahasiswa ini statusnya : $isActive = aktive");
+}
---
+++ Tugas_i/bin/5IF3/latihan/latihan2.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  int panjang = 10;
+  int lebar = 5;
+  int luas = panjang * lebar;
+  int keliling = panjang + lebar;
+  print("Luas persegi panjang = $luas");
+  print("Keliling persegi panjang = $keliling");
+}
