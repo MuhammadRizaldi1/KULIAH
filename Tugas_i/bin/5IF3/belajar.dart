bd74df97786dfe07eb642d3409216628becddd86
MuhammadRizaldi1 <mhmdrizaldi123p@gmail.com>
2025-09-04 08:37:38 +0900

    Upload Tugas2

---
+++ Tugas_i/bin/5IF3/belajar.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  Set<int> angka = {1, 2, 3, 4, 5};
+  print(angka.length);
+  print(angka.add(2));
+  print(angka.remove(4));
+  print(angka.contains(4));
+  angka.clear();
+}
